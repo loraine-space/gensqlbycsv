ebf4a71e59ce5bb593c9a7b1c2fdcf87cb6b45f2
rainofsilence <822466858@qq.com>
2022-08-03 21:22:43 +0800

    init

---
+++ src/main/java/cn/rofs/Main.java
@@ -0,0 +1,11 @@
+package cn.rofs;
+
+/**
+ * @author rainofsilence
+ * @date 2022/8/3 周三
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
