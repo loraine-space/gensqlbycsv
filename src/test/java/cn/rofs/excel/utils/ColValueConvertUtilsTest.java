2c618af814cc29ba806a0b997b2e23afe0c02846
rainofsilence <822466858@qq.com>
2022-08-18 08:56:47 +0800

    [v1.0.1] Add ColValueConvertUtilsTest

---
+++ src/test/java/cn/rofs/excel/utils/ColValueConvertUtilsTest.java
@@ -0,0 +1,17 @@
+package cn.rofs.excel.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rainofsilence
+ * @date 2022/8/18 周四
+ */
+class ColValueConvertUtilsTest {
+
+    @Test
+    void convertWithCol() {
+        ColValueConvertUtils.convertWithCol(null);
+    }
+}
